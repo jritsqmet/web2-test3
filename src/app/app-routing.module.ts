023909cbcbe214e21c79431878f4ca06d176516d
paulrrm <paulruizm@gmail.com>
2023-11-18 15:14:09 -0500

    estructura base y ejemplo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { EjemploComponent } from './pages/ejemplo/ejemplo.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { TerminosComponent } from './pages/terminos/terminos.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"acercade",component: AcercadeComponent},
+  {path:"carrito",component: CarritoComponent},
+  {path:"contacto",component: ContactoComponent},
+  {path:"detalles",component: DetalleComponent},
+  {path:"ejemplo",component: EjemploComponent},
+  {path:"",component: HomeComponent},
+  {path:"ofertas",component: OfertasComponent},
+  {path:"productos",component: ProductosComponent},
+  {path:"terminos",component: TerminosComponent},
+  {path:"**",component: Error404Component},
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { OfertasComponent } from './pages/ofertas/ofertas.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { TerminosComponent } from './pages/terminos/terminos.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FutterComponent } from './components/futter/futter.component';
+import { EjemploComponent } from './pages/ejemplo/ejemplo.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AcercadeComponent,
+    CarritoComponent,
+    ContactoComponent,
+    DetalleComponent,
+    OfertasComponent,
+    ProductosComponent,
+    TerminosComponent,
+    NavBarComponent,
+    FutterComponent,
+    EjemploComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
